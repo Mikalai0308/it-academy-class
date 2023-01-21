61f12f79a76eca04ec3f8f9abdc8373d9f47afe5
Mikalai Kalchuk <knik.f1@gmail.com>
2023-01-21 13:48:04 +0300

    лекция2

---
+++ script.js
@@ -0,0 +1,19 @@
+const undreadMessage = 10;
+const regUser = 'yes';
+const favoriteGenre = 'rock';
+
+let ADMIN = 'admin';
+let ANONIMUS = 'anonimus';
+let user = 'anonimus';
+
+if (user === 'admin') {
+    console.log(ADMIN)
+}
+else {console.log(ANONIMUS)}
+
+let k='#';
+for (let i=0; i<7; i=i+1) {
+console.log(k);
+k=k+'#'
+};
+
